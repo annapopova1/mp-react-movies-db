c7944bdabde6aaa68f9e6c9161528ef372f7e188
Anna Popova <anna_popova1@epam.com>
2018-05-20 13:18:31 +0300

    Persisted state to local storage

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './components/app/app';
 import './main.css';
-import getStore from './store/store';
+import getStoreConfig from './store/storeConfig';
+
+const storeConfig = getStoreConfig();
 
 render(
-  <Provider store={getStore()}>
-    <App />
+  <Provider store={storeConfig.store}>
+    <PersistGate loading={null} persistor={storeConfig.persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('movie-db-app'),
 );
