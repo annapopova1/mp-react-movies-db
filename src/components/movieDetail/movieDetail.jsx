552c77dbae04688eda6ee12d2b54bcf10f345b47
Anna Popova <anna_popova1@epam.com>
2018-05-27 20:27:22 +0300

    Removed not necessary image's styles and fixed some template conditions

--- src/components/movieDetail/movieDetail.jsx
+++ src/components/movieDetail/movieDetail.jsx
@@ -10,15 +10,13 @@ const MovieDetail = ({ movie }) => (
       <div className="card-body d-flex flex-column align-items-start">
         <h3 className="card-title">
           {movie.title}
-          { movie.voteAvg && <span className="badge badge-secondary ml-4 rating">{movie.voteAvg}</span> }
+          {movie.voteAvg > 0 && <span className="badge badge-secondary ml-4 rating">{movie.voteAvg}</span>}
         </h3>
-        {/* <p className="card-subtitle mb-2 text-muted">This is a wider card with supporting.</p> */}
         <p className="card-text text-muted">
           <span>{movie.releaseYear}</span>
-          {movie.runtime && <span className="ml-5">{movie.runtime} min</span>}
+          {movie.runtime > 0 && <span className="ml-5">{movie.runtime} min</span>}
         </p>
         <p className="card-text">{movie.overview}</p>
-
       </div>
     </div>
   </section>
